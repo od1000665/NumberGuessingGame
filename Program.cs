6b7bfc9f81804b391053d2454d6426038817b391
od1000665 <od1000665@otc.edu>
2025-09-30 09:57:09 -0500

    Initial Commit
    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Number Guessing Game
+    Created by: Owen Douglas
+    A simple game with 3 difficulties and will tell the user if they are close to the number
+*/
